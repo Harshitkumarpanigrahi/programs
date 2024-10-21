b85225f5df7d8fbb30fe4833e4b803222eec85d0
Harshitkumarpanigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-21 23:04:51 +0530

    Create Lucky Seven.py

---
+++ Lucky Seven.py
@@ -0,0 +1,11 @@
+
+Chef considers the number 77 lucky. As a result, he believes that the 7-th letter he sees on a day is his lucky letter of the day.
+You are given a string SS of length 10
+10, denoting the first 
+10
+10 letters Chef saw today.
+What is Chef's lucky letter?
+
+
+s=input()
+print(s[6])
