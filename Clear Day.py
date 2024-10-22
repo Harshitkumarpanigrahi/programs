c4e8f8297427171acc2ea2899b54c8326c4d354e
Harshitkumarpanigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-22 21:47:33 +0530

    Create Clear Day.py

---
+++ Clear Day.py
@@ -0,0 +1,12 @@
+
+Chef classifies a day to be either rainy, cloudy, or clear.
+In a particular week, Chef finds X
+X days to be rainy and Y
+Y days to be cloudy.
+Find the number of clear days in the week.
+
+
+
+x,y=map(int,input().split())
+clear=7-x-y
+print(clear)
