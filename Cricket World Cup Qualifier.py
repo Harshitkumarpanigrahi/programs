748c1c6d8e4b7b775bb94cd6bf80c87c8396a722
Harshitkumarpanigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-22 21:48:31 +0530

    Create Cricket World Cup Qualifier.py

---
+++ Cricket World Cup Qualifier.py
@@ -0,0 +1,13 @@
+
+The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches. Any team that scores 
+12
+12 or more points in the group stage matches qualifies for the next stage.
+
+You know the score that a particular team has scored in the group stage matches. Determine if the team has qualified for the next stage or not.
+
+x=int(input())
+if x>=12:
+    print("Yes")
+else:
+    print("No")
+
