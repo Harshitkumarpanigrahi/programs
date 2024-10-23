4d2b4dd351d8830965b270b225c4c402f861f8b8
Harshitkumarpanigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-23 23:05:54 +0530

    Create Masterchef finals .py

---
+++ Masterchef finals .py
@@ -0,0 +1,23 @@
+Chef has been working hard to compete in MasterChef.
+He is ranked 
+X
+X out of all contestants. However, only 
+10
+10 contestants would be selected for the finals.
+
+Check whether Chef made it to the top 
+10
+10 or not?
+
+
+x=int(input())
+for i in range(x):
+    n=int(input())
+    if n>10:
+         print("NO")
+    elif n!=10 == 50:
+        print("")
+    else:
+        print("YES")
+
+    
