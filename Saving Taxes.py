7e2da7703faa260bfdf26fdfe33190355bde19a0
Harshitkumarpanigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-23 23:07:59 +0530

    Create Saving Taxes.py

---
+++ Saving Taxes.py
@@ -0,0 +1,16 @@
+
+In Chefland, everyone who earns strictly more than Y
+rupees per year, has to pay a tax to Chef. Chef has allowed a special scheme where you can invest any amount of money and claim exemption for it.
+
+You have earned X
+ x(X>Y) rupees this year. Find the minimum amount of money you have to invest so that you don't have to pay taxes this year.
+
+
+
+t=int(input())
+for i in range(t):
+    x,y=map(int,input().split())
+    print(x-y)
+    
+
+
