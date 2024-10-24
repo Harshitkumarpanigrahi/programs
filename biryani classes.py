8acf599486ce3d19d00adeecc5c2dc522ff00593
Harshit Kumar Panigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-24 21:56:33 +0530

    Create biryani classes.py

---
+++ biryani classes.py
@@ -0,0 +1,14 @@
+According to a recent survey, Biryani is the most ordered food. Chef wants to learn how to make world-class Biryani from a MasterChef. Chef will be required to attend the MasterChef's classes for 
+X
+X weeks, and the cost of classes per week is 
+Y
+Y coins. What is the total amount of money that Chef will have to pay?
+
+
+
+t=int(input())
+for i in range(t):
+    x,y=map(int,input().split())
+    res=x*y
+    print(res)
+    
