29f352d7f63af3ae1b6f42362f6c4ce9798559d0
Harshit Kumar Panigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-25 22:29:39 +0530

    Create Double Rent.py

---
+++ Double Rent.py
@@ -0,0 +1,12 @@
+
+Chefina decided to move into Chef's apartment.
+Chef was initially paying a rent of 
+X
+X rupees. Since Chefina is moving in, the owner decided to double the rent.
+
+Find the final rent Chef needs to pay.
+
+
+  
+  x=int(input())
+print(x+x)
