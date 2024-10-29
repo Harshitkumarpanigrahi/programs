a4be48cb4222d583ad0a815588b7d986f7eb7531
Harshit Kumar Panigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-30 01:13:13 +0530

    Create Reach on Time.py

---
+++ Reach on Time.py
@@ -0,0 +1,18 @@
+
+Chef has recently moved into an apartment. It takes 
+30
+30 minutes for Chef to reach office from the apartment.
+
+Chef left for the office 
+X
+X minutes before Chef was supposed to reach. Determine whether or not Chef will be able to reach on time.
+
+
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+    if n>=30:
+        print("YES")
+    else:
+        print("NO")
