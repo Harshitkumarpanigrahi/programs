f94d4c1d54a4b7144ebed8d7f7ca7b4c49c6b225
Harshit Kumar Panigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-10-30 17:44:54 +0530

    Create 2000.py

---
+++ 2000/2000.py
@@ -0,0 +1,23 @@
+
+Chef had collected 
+N
+N notes of Rs. 
+2000
+2000 to pay his total college fees. However, the government banned Rs. 
+2000
+2000 notes.
+
+Chef wants to pay the same amount using Rs. 
+500
+500 notes only. Find the number of notes Chef needs.
+
+
+
+n=int(input())
+x=n*2000
+y=x//500
+print(y)
+
+
+
+
