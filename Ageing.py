e121fe99ec6f8991a3c25ca4f8befef4aa4abc0d
Harshit Kumar Panigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-11-05 21:10:44 +0530

    Create Ageing.py

---
+++ Ageing.py
@@ -0,0 +1,31 @@
+
+Chef's current age is 
+20
+20 years, while Chefina's current age is 
+10
+10 years.
+Determine Chefina's age when Chef will be 
+X
+X years old.
+
+Note: Assume that Chef and Chefina were born on same day and same month (just different year).
+
+Input Format
+The first line of input will contain a single integer 
+T
+T, denoting the number of test cases.
+Each test case consists of a single integer 
+X
+X, the age of Chef.
+Output Format
+For each test case, output Chefina's age when Chef will be 
+X
+X years old.
+
+
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+    z=n-10
+    print(z)
