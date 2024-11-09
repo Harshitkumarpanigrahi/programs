82ab12719783f6085ccf8c4833ceb89c119c5823
Harshit Kumar Panigrahi <166209709+Harshitkumarpanigrahi@users.noreply.github.com>
2024-11-09 20:23:26 +0530

    Create Water Requirement.py

---
+++ Water Requirement.py
@@ -0,0 +1,41 @@
+Finally, after purchasing a water cooler during the April long challenge, Chef noticed that his water cooler requires 
+2
+2 liters of water to cool for one hour.
+
+How much water (in liters) would be required by the cooler to cool for 
+N
+N hours?
+
+Input Format
+The first line of input will contain an integer 
+T
+T — the number of test cases. The description of 
+T
+T test cases follows.
+The first and only line of each test case contains an integer 
+N
+N, as described in the problem statement.
+Output Format
+For each test case, output the number of liters of water required by the water cooler to cool for 
+N
+N hours.
+
+Constraints
+1
+≤
+T
+≤
+1000
+1≤T≤1000
+1
+≤
+N
+≤
+1000
+1≤N≤1000
+
+t=int(input())
+for i in range(t):
+    x=int(input())
+    res=x+x
+    print(res)
